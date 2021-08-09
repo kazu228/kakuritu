2e684ab676daec4a47ecd5287e7fb1a793363291
kazuaki <kskkm022@gmail.com>
2021-08-09 17:15:50 +0900

    calculate関数を追加

--- cal.py
+++ cal.py
@@ -13,6 +13,17 @@ def draw_down_line(count, diverted_flag=''):
     for i in range(count):
         print("|", end=diverted_flag)
 
+def calculate():
+    result = 0
+    for i in range(1, 7):
+        for j in range(1, 7):
+            result = i + j
+            if j == 6:
+                print(result)
+            else:
+                print(result, end="")
+
+
 print(" ", end="")
 for num in range(1, 7):
     draw_down_line(1)
@@ -26,3 +37,6 @@ draw_across_line(14)
 for num in range(1, 7):
     print(num, end="")
     draw_down_line(1, "\n")
+
+
+calculate()
