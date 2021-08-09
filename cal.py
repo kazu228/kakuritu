e74f319ffe281d5ff0f5db444d8f80874cc833fa
kazuaki <kskkm022@gmail.com>
2021-08-09 17:52:12 +0900

    数字を埋め込む

--- cal.py
+++ cal.py
@@ -13,15 +13,14 @@ def draw_down_line(count, diverted_flag=''):
     for i in range(count):
         print("|", end=diverted_flag)
 
-def calculate():
+def calculate(i):
     result = 0
-    for i in range(1, 7):
-        for j in range(1, 7):
-            result = i + j
-            if j == 6:
-                print(result)
-            else:
-                print(result, end="")
+    for j in range(1, 7):
+        result = i + j
+        if j == 6:
+            print(str(result) + " ")
+        else:
+            print(str(result) + " ", end="")
 
 
 print(" ", end="")
@@ -36,7 +35,5 @@ draw_across_line(14)
 
 for num in range(1, 7):
     print(num, end="")
-    draw_down_line(1, "\n")
-
-
-calculate()
+    draw_down_line(1)
+    calculate(num)
